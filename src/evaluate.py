8443fb1f943ed0d960e7b7dc35b0de84f3159905
Mariem-Nasri <mariem.nasri773@gmail.com>
2025-03-05 08:34:10 +0100

    update jenkinsfile

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,22 +2,6 @@
 import mlflow
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import logging
-from elasticsearch import Elasticsearch
-
-
-es = Elasticsearch(
-    "https://localhost:9200",
-    basic_auth=("elastic", "elastic"),  # Use `basic_auth` instead of `http_auth`
-    verify_certs=False
-)
-
-
-class ElasticsearchHandler(logging.Handler):
-    def emit(self, record):
-        log_entry = self.format(record)
-        es.index(index='mlflow-metrics', document={'message': log_entry})
-        if not es.indices.exists(index='mlflow-metrics'):
-            es.indices.create(index='mlflow-metrics')
 
 
 def evaluate_model(model, X_test_scaled, y_test):
@@ -33,8 +17,6 @@ def evaluate_model(model, X_test_scaled, y_test):
     mlflow.log_metric("f1_score", f1)
     # Log metrics to Elasticsearch
     logger = logging.getLogger(__name__)
-    es_handler = ElasticsearchHandler()
-    logger.addHandler(es_handler)
     logger.info(f"Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, F1-score: {f1}")  # ADD F1-score ✅
 
     return {'accuracy': accuracy, 'precision': precision, 'recall': recall, 'f1_score': f1}  # RETURN F1 ✅
